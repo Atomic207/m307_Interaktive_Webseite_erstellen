c6f31454d9820098e8256053e480e493471eda79
AlineTheytaz <aline.theytaz@sbw-media.ch>
2023-11-15 09:25:58 +0100

    nei danke

--- index.php
+++ index.php
@@ -10,14 +10,17 @@
     <label for="vorname"></label><br>
     <input type="vorname" name="vorname" id="vorname" placeholder=Vorname><br>
      <label for="nachname"></label><br>
-     <input type="nachname" name="nachname" id="nachname" placeholder=Nachname><br>   
-    <label for="lehrgang">Lehrgang:</label>
+     <input type="nachname" name="nachname" id="nachname" placeholder=Nachname><br> 
+     <label for="email"></label><br>
+        <input type="email" name="email" id="email" placeholder=Email><br>
+    <label for="lehrgang"></label> <br>
     <select name="lehrgang" id="lehrgang">
+        <option value="Lehrgang">Lehrgang</option>
         <option value="2021">2021</option>
         <option value="2022">2022</option>
         <option value="2023">2023</option>
-        <option value="2024">2024</option>
-    </select>
+        <option value="2024">2024</option> 
+    </select> <br> <br>
     
         <input type="submit" value="Senden">
     </form>
