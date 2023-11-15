2d2f83c124e0069ef79135e5182e99a8894feadf
AlineTheytaz <aline.theytaz@sbw-media.ch>
2023-11-15 08:30:57 +0100

    step1

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form DB</title>
 </head>
 <body>
-    <h1>okokok</h1>
-    <h2>tschau</h2>
-    <p>kok</p>
+    <h2>Anmeldung</h2>
+    <form action="index-save.php" method="post">
+        <label for="vorname">Vorname</label><br>
+        <input type="vorname" name="vorname" id="vorname"><br>
+        <label for="nachname">Nachname</label><br>
+        <input type="nachname" name="nachname" id="nachname"><br>
+        <div class="dropdown">
+
+        
+        <input type="submit" value="Senden">
+    </form>
 </body>
 </html>
