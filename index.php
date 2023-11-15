7ffccaa0d1fea5f507564764e906c871005eb7b8
Atomic207 <elijah.rietmann@icloud.com>
2023-11-15 09:06:16 +0100

    aline warum

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <body>
     <h2>Anmeldung</h2>
     <label for="vorname">Vorname</label><br>
-    <input type="vorname" name="vorname" id="vorname"><br>
-     <label for="nachname">Nachname</label><br>
-     <input type="nachname" name="nachname" id="nachname"><br>   
+    <input type="vorname" name="vorname" id="vorname" placeholder=Vorname><br>
+     <label for="nachname"></label><br>
+     <input type="nachname" name="nachname" id="nachname" placeholder=Nachname><br>   
     <label for="lehrgang">Lehrgang:</label>
     <select name="lehrgang" id="lehrgang">
         <option value="2021">2021</option>
