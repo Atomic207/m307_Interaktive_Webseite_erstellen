d4e4db3477241ea63fc4a21cf54cda29fd05af24
Atomic207 <elijah.rietmann@icloud.com>
2023-11-15 08:50:18 +0100

    dropdown added

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 </head>
 <body>
     <h2>Anmeldung</h2>
-    <label for="dog-names">Choose a dog name:</label> 
-    <select name="dog-names" id="dog-names"> 
-        <option value="rigatoni">Rigatoni</option> 
-        <option value="dave">Dave</option> 
-        <option value="pumpernickel">Pumpernickel</option> 
-        <option value="reeses">Reeses</option> 
+    <label for="lehrgang">Lehrgang:</label> 
+    <select name="lehrgang" id="lehrgang"> 
+        <option value="2021">2021</option> 
+        <option value="2022">2022</option> 
+        <option value="2023">2023</option> 
+        <option value="2024">2024</option> 
     </select>
 
         <input type="submit" value="Senden">
