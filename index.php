877f754b0afbde6aeda87694b6d82a8ec9a02b05
Atomic207 <elijah.rietmann@icloud.com>
2023-11-15 09:44:44 +0100

    i wanna unalive myself

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
     <title>Form DB</title>
 </head>
 <body>
+<div>
     <h2>Anmeldung</h2>
     <label for="vorname"></label><br>
     <input type="vorname" name="vorname" id="vorname" placeholder=Vorname><br>
      <label for="nachname"></label><br>
      <input type="nachname" name="nachname" id="nachname" placeholder=Nachname><br> 
      <label for="email"></label><br>
-        <input type="email" name="email" id="email" placeholder=Email><br>
+    <input type="email" name="email" id="email" placeholder=Email><br>
+</div>
+<div>
     <label for="lehrgang"></label> <br>
     <select name="lehrgang" id="lehrgang">
         <option value="Lehrgang">Lehrgang</option>
@@ -21,7 +24,16 @@
         <option value="2023">2023</option>
         <option value="2024">2024</option> 
     </select> <br> <br>
-    
+
+</div>
+
+<div>
+<input type="checkbox" class="radio" value="1" name="geschlecht" />Männlich</label> <br>
+  <label>
+    <input type="checkbox" class="radio" value="1" name="geschlecht" />Weiblich</label> <br>
+  <label>
+    <input type="checkbox" class="radio" value="1" name="geschlecht" />Divers</label> <br>
+</div>
         <input type="submit" value="Senden">
     </form>
 </body>
