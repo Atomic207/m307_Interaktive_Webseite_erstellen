ace3fdc8d42b0e09dd389d8f520592a738b1a3dd
Atomic207 <elijah.rietmann@icloud.com>
2023-11-15 09:04:43 +0100

    test dis

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team & Rollen</title>
+    <title>Form DB</title>
 </head>
 <body>
     <h2>Anmeldung</h2>
-    <label for="vorname"></label><br>
+    <label for="vorname">Vorname</label><br>
     <input type="vorname" name="vorname" id="vorname"><br>
-     <label for="nachname"></label><br>
+     <label for="nachname">Nachname</label><br>
      <input type="nachname" name="nachname" id="nachname"><br>   
-    <label for="lehrgang">Lehrgang:</label> 
-    <select name="lehrgang" id="lehrgang"> 
-        <option value="2021">2021</option> 
-        <option value="2022">2022</option> 
-        <option value="2023">2023</option> 
-        <option value="2024">2024</option> 
+    <label for="lehrgang">Lehrgang:</label>
+    <select name="lehrgang" id="lehrgang">
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+        <option value="2023">2023</option>
+        <option value="2024">2024</option>
     </select>
-
+    
         <input type="submit" value="Senden">
     </form>
 </body>
