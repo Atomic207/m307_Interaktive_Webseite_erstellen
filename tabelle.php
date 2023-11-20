b56146221864f17bd582a94f096f79a21a20f0f3
Atomic207 <elijah.rietmann@icloud.com>
2023-11-20 13:55:03 +0100

    han e Tabelle gmacht

--- tabelle.php
+++ tabelle.php
@@ -10,44 +10,44 @@
 .tg  {border-collapse:collapse;border-spacing:0;}
 .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
 .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
-.tg .tg-3zav{font-size:13px}
+.tg .sized{font-size:13px}
 </style>
 <table class="tg">
   <tr>
-    <th class="tg-3zav">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</th>
-    <th class="tg-031e"></th>
-    <th class="tg-031e"></th>
-    <th class="tg-031e"></th>
+    <th class="sized"></th>
+    <th class="sized"></th>
+    <th class="sized"></th>
+    <th class="sized"></th>
   </tr>
   <tr>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
   </tr>
   <tr>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
   </tr>
   <tr>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
   </tr>
   <tr>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
   </tr>
   <tr>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
-    <td class="tg-031e"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
+    <td class="sized"></td>
   </tr>
 </table>
 </body>
