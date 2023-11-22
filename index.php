7a4611137849fdce2aa65628d678ba3537cd5ea6
GiorgiaIrrera <Giorgia.Irrera@sbw-media.ch>
2023-11-22 09:18:03 +0100

    hhhh

--- index.php
+++ index.php
@@ -7,12 +7,7 @@
     <link rel="stylesheet" href="/style/style.css">
 </head>
 <body>
-<style type="text/css">
-.tg  {border-collapse:collapse;border-spacing:0;}
-.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
-.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
-.tg .sized{font-size:13px}
-</style>
+
 <table class="tg">
   <tr>
     <th class="sized"></th>
@@ -51,7 +46,7 @@
     <td class="sized"></td>
   </tr>
 </table>
-<a href="addnew">
+<a href="addnew.php">
             <input type="submit" value="Einen weiteren LB hinzufügen" class="full-width green-bg">
             </a>
 </body>
