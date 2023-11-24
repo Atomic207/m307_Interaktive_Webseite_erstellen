47f1874550b268959319824e58fed71438217790
AlineTheytaz <aline.theytaz@sbw-media.ch>
2023-11-24 11:20:06 +0100

    pro simmer

--- index-save.php
+++ index-save.php
@@ -1,8 +1,45 @@
 <?php
+$nachname = $_POST['nachname'] ??'';
+$vorname = $_POST['vorname'] ??'';
+$email = $_POST['email'] ??'';
+$lehrgang = $_POST['lehrgang'] ??'';
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    /*db verbinden*/
+    $server ='localhost';
+    $dbname = 'praxisarbeit';
+    $dbuser = 'm307';
+    $dbpass = 'm307';
 
-$sql = "INSERT INTO `team-rollen` (`ID`, `Nachname`, `Vorname`, `Lehrbeginn`)
- VALUES (NULL, ?, ?, ?);";
-$stmt = $db -> prepare($sql);
-$result = $stmt -> execute([ ]);
+    $db = new PDO(
+        "mysql:dbname=$dbname;host=$server",
+        $dbuser,
+        $dbpass
+    );
+
+    /*sql befehl vorbereiten*/
+    $sql = "INSERT INTO `team-rollen` (`ID`, `Nachname`, `Vorname`, `email`, `Lehrbeginn`) VALUES (NULL, ':nachname', ':vorname', ':email', ':lehrgang')";
+    $stmt = $db -> prepare($sql);
+
+    /*sql befehl ausführen*/
+    $result = $stmt -> execute([
+        "nachname" => $vorname,
+        "vorname" => $nachname,
+        "email" => $email,
+        "lehrgang" => $lehrgang
+    ]);
+    $kommentar_id = $db-> lastInsertId();
+    /*resultat ausgeben*/
+    if($result){
+        header("Location: succsess.php?kommentar-id=$kommentar_id");
+    }
+        
+    else{
+        header("Location: index.php");
+    }
+}
+else{
+    header("Location: addnew.php");
+}
+?>
 
